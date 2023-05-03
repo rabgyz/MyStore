85162f814cc40877234e1adfd0f7494c0a4379c9
Rabgyz <rabgyz@gmail.com>
2023-05-03 18:36:51 +0200

    Pushing main project files

---
+++ MyStore/Pages/Clients/Index.cshtml.cs
@@ -0,0 +1,58 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Data.SqlClient;
+
+namespace MyStore.Pages.Clients
+{
+    public class IndexModel : PageModel
+    {
+        public List<ClientInfo> clientList = new List<ClientInfo>();
+        public void OnGet()
+        {
+            try
+            {
+                string connectionString = "Data Source=.\\sqlexpress;Initial Catalog=MyStore;Integrated Security=True";
+
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    string sql = "SELECT * FROM clients";
+
+                    using (SqlCommand command = new SqlCommand(sql, connection))
+                    {
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                ClientInfo clientInfo = new ClientInfo();
+                                clientInfo.id = "" + reader.GetInt32(0);
+                                clientInfo.name = reader.GetString(1);
+                                clientInfo.email = reader.GetString(2);
+                                clientInfo.phone = reader.GetString(3);
+                                clientInfo.address = reader.GetString(4);
+                                clientInfo.created_at = reader.GetDateTime(5).ToString();
+
+                                clientList.Add(clientInfo);
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Error while loading a records");
+                throw;
+            }
+        }
+    }
+
+    public class ClientInfo
+    {
+        public string id;
+        public string name;
+        public string email;
+        public string phone;
+        public string address;
+        public string created_at;
+    }
+}
